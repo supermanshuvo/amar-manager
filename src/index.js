a9afc9d0bee36eddd7c6b3c3d8e7adbddb5d3444
supermanshuvo <shuvothesuperman@gmail.com>
2021-05-22 22:23:53 +0600

    Create Product component for  producta table

--- src/index.js
+++ src/index.js
@@ -4,16 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header';
 
-/* ReactDOM.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-); */
-
-ReactDOM.render( <Header />, document.getElementById("root"));
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
