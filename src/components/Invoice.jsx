395468e452c0d095d0e5e69a3e848f7ec35fc2c1
supermanshuvo <shuvothesuperman@gmail.com>
2021-05-26 02:23:47 +0600

    Create some function for the invoice list

--- src/components/Invoice.jsx
+++ src/components/Invoice.jsx
@@ -1,7 +1,11 @@
 import { Link } from 'react-router-dom';
 const Invoice = () => {
-    return (
-        <div className="container">
+    var count = 0;
+    var invoice = localStorage.getItem("invoice");
+    var invoice = JSON.parse(invoice);
+    if(!invoice.length){
+        return(
+            <div className="container">
             <Link className="btn btn-success" to="/product">CREATE NEW INVOICE</Link>
             <div className="table mt-4 font-weight-light">
                 <table class="table">
@@ -16,31 +20,70 @@ const Invoice = () => {
                             <th scope="col">Amount</th>
                         </tr>
                     </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="row">1</th>
-                            <td>Asif Salman Malik</td>
-                            <td>01770810050</td>
-                            <td>22/05/2021</td>
-                            <td>03:16 PM</td>
-                            <td>2</td>
-                            <td>৳ 175.00</td>
-                        </tr>
-                        <tr>
-                            <th scope="row">2</th>
-                            <td>Shuvo</td>
-                            <td>01303316865</td>
-                            <td>02/05/2021</td>
-                            <td>02:16 PM</td>
-                            <td>1</td>
-                            <td>৳ 115.00</td>
-                        </tr>
-                    </tbody>
                 </table>
+                        <h3 colspan="4" className="text-center">There is no data</h3>
                 <Link className="btn btn-primary" to="/print">PRINT INVOICE</Link>
             </div>
         </div>
-    );
+        );
+    }else{
+        return (
+            <div className="container">
+                <Link className="btn btn-success" to="/product">CREATE NEW INVOICE</Link>
+                <div className="table mt-4 font-weight-light">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Invoice ID</th>
+                                <th scope="col">Customer Name</th>
+                                <th scope="col">Contact No.</th>
+                                <th scope="col">Date</th>
+                                <th scope="col">Time</th>
+                                <th scope="col">Products</th>
+                                <th scope="col">Amount</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Asif Salman Malik</td>
+                                <td>01770810050</td>
+                                <td>22/05/2021</td>
+                                <td>03:16 PM</td>
+                                <td>2</td>
+                                <td>৳ 175.00</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Shuvo</td>
+                                <td>01303316865</td>
+                                <td>02/05/2021</td>
+                                <td>02:16 PM</td>
+                                <td>1</td>
+                                <td>৳ 115.00</td>
+                            </tr>
+                            {invoice.map(invoice => {
+                                console.log(count);
+                                return(
+                                    <tr>
+                                        <th scope="row">{invoice.invoice_id}</th>
+                                        <td>{invoice.invoice_customer.name}</td>
+                                        <td>{invoice.invoice_customer.mobile}</td>
+                                        <td>{invoice.invoice_date}</td>
+                                        <td>{invoice.invoice_time}</td>
+                                        <td>{invoice.invoice_items[count].cart_qty}</td>
+                                        <td>৳ {(invoice.invoice_items[count].product_sale * invoice.invoice_items[count].cart_qty)}</td>
+                                    </tr>
+
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                    <Link className="btn btn-primary" to="/print">PRINT INVOICE</Link>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Invoice;
--- src/components/Print.jsx
+++ src/components/Print.jsx
@@ -1,65 +1,52 @@
 import { Link } from 'react-router-dom';
+import React, { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
+
+import { PrintArea } from './PrintArea';
+
 const Print = () => {
-  const invoicePdf= () => {
-    var printContents = document.getElementById("printArea").innerHTML;
-			var originalContents = document.body.innerHTML;
-			document.body.innerHTML = printContents;
-			window.print();
-			document.body.innerHTML = originalContents;
-  }
-    return(
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  return (
         <div className="container">
-          <div id="printArea">
-            <div className="text-center">
-                <h2 className="font-weight-bold">A. R. Electric & Electronics</h2>
-                <p>2F, Porimal Tower, N.S. Road, Kushtia - 7400. <br />Contact: 01711-236456, 01848-455987</p>
-                <p>Invoice No# <span className="font-weight-bold">3</span></p>
-            </div>
-            <div className="customerName">
-                <p><span className="font-weight-bold">Name:</span> Asif Salman Malik - 01770810050</p>
-                <p><span className="font-weight-bold">Address:</span> Babu Para, Alamdanga, Chuadanga.</p>
-            </div>
-            <div className="invoiceTablePrint">
-                <h4 className="text-center font-weight-bold mb-4">Sales Invoice</h4>
-                <table class="table">
-                <thead>
-                  <tr>
-                  <th scope="col">Product Name</th>
-                    <th scope="col">Quantity</th>
-                    <th scope="col">Sale Rate</th>
-                    <th scope="col">Sub Total</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <th scope="row">Pran Chinigura Rice - 1KG</th>
-                      <td>19 Pcs</td>
-                      <td>৳ 108</td>
-                      <td>৳ 120</td>
-                  </tr>
-                  <tr className="font-weight-bold">
-                    <th>Total Items: 1</th>
-                    <td colspan="2">Total</td>
-                    <td>৳ 35.00</td>
-                  </tr>
-                  <tr>
-                    <td colspan="3">Discount</td>
-                    <td>৳ 0.00</td>
-                  </tr>
-                  <tr>
-                    <td colspan="3">Grand Total</td>
-                    <td>৳ 105.00</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
-            <div className="button">
+          <PrintArea ref={componentRef} />
+            <div className="button mb-3">
                 <Link className="btn btn-primary" to="/invoice"><i className="fa fa-long-arrow-alt-left"></i> BACK TO INVOICE LIST</Link>
-                <button onClick={invoicePdf} className="btn btn-success float-right"><i className="fa fa-print"></i> PRINT INVOICE</button>
+                <button onClick={handlePrint} className="btn btn-success float-right"><i className="fa fa-print"></i> PRINT INVOICE</button>
             </div>
         </div>
     );
 }
 
 export default Print;
+
+
+
+/* 
+
+import React, { useRef } from 'react';
+import { useReactToPrint } from 'react-to-print';
+
+import { PrintArea } from './PrintArea';
+
+const Print = () => {
+  const componentRef = useRef();
+  const handlePrint = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  return (
+    <div>
+      <PrintArea ref={componentRef} />
+      <button onClick={handlePrint}>Print this out!</button>
+    </div>
+  );
+};
+
+export default Print;
+
+
+*/
