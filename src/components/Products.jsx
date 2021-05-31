0720602a233ca5b7501970022be47ff20597949e
supermanshuvo <shuvothesuperman@gmail.com>
2021-05-31 22:36:27 +0600

    add checkbox to select data

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
 import { useMemo } from 'react';
-import { useTable } from 'react-table';
+import { useTable, useRowSelect } from 'react-table';
 import Order from './Order';
+import { Checkbox } from './Checkbox'
+
 const Products = () => {
   var customerData = localStorage.getItem("customer");
   var customer = JSON.parse(customerData);
@@ -33,18 +35,35 @@ const Products = () => {
                 getTableBodyProps,
                 headerGroups,
                 rows,
-                prepareRow
+                prepareRow,
+                selectedFlatRows
               } = useTable({
                 columns,
                 data,
-            }) 
+            },
+            useRowSelect,
+            hooks => {
+              hooks.visibleColumns.push(columns => [
+                {
+                  product_name : 'selection',
+                  Header: ({ getToggleAllRowsSelectedProps }) => (
+                    <Checkbox {...getToggleAllRowsSelectedProps()} />
+                  ),
+                  Cell: ({ row }) => <Checkbox {...row.getToggleRowSelectedProps()} />
+                },
+                ...columns
+              ])
+            }
+          )
+    const selectedItem = selectedFlatRows.map(row => row.original)
+    console.log(selectedItem);
     return (
       <div>
         <Link className="btn btn-success btn-sm" to="/invoice">INVOICE LIST</Link>
         <div className="row mt-2">
           <div className="col-md-5">
           <table className="table table-hover" {...getTableProps()}>
-                <thead>
+                <thead className="bg-primary text-white">
                 {headerGroups.map(headerGroup => (
                     <tr {...headerGroup.getHeaderGroupProps()}>
                     {headerGroup.headers.map(column => (
@@ -88,11 +107,23 @@ const Products = () => {
               </div>
             </div>
             <div className="col-*-12 mt-4">
-                <Order />
+                <Order selectedItem />
               <Link className="btn btn-success btn-sm" to="/invoice">CREATE INVOICE</Link>
             </div>
           </div>
         </div>
+
+        <pre>
+        <code>
+          {JSON.stringify(
+            {
+              selectedItem: selectedFlatRows.map(row => row.original)
+            },
+            null,
+            2
+          )}
+        </code>
+      </pre>
       </div>
     );
 }
